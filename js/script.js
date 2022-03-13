6c888e6e68b84ba30e810788f95e2fb412ec1c09
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-03-14 00:48:11 +0300

    feature:mail chimp

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
-    $(".design-two").hide();
     $(".design-one").click(function(){
         $(".design-one").toggle();
         $(".design-two").toggle();
     })
-    $(".design-one").mouseleave(function(){
-        $(".design-two").hide()
-        $(".design-one").show();
+    $(".design-two").click(function(){
+        $(".design-one").toggle();
+        $(".design-two").toggle();
     })
+    // $(".design-one").mouseleave(function(){
+    //     $(".design-two").hide()
+    //     $(".design-one").show();
+    // })
     $(".design-four").hide();
     $(".design-three").click(function(){
+        $(".design-three").toggle();
+        $(".design-four").toggle();
+    })
+    $(".design-four").click(function(){
         $(".design-three").toggle();
         $(".design-four").toggle();
     })
@@ -18,37 +25,15 @@ $(document).ready(function(){
         $(".design-five").toggle();
         $(".design-six").toggle();
     })
+    $(".design-six").hide();
+    $(".design-six").click(function(){
+        $(".design-five").toggle();
+        $(".design-six").toggle();
+    })
 
 
 
- $(".work1").hover(function(){
-     $(this).css("opacity","0,9");
-     $("h2").css("color","blue")
-     
- })
- $(".work2").hover(function(){
-    $(this).css("border","solid 20px purple");
-    $()
-})
-$(".work3").hover(function(){
-    $(this).css("background-color","yellow");
-})
-$(".work4").hover(function(){
-    $(this).css("background-color","yellow");
-})
-$(".work5").hover(function(){
-    $(this).css("background-color","yellow");
-})
-$(".work6").hover(function(){
-    $(this).css("background-color","yellow");
-})
-$(".work7").hover(function(){
-    $(this).css("background-color","yellow");
-})
-$(".work8").hover(function(){
-    $(this).css("background-color","yellow");
-})
-
+ 
 $("button#button").submit(function(){
 
 })
