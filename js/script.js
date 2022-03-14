73bb1b34ced06b982a3d89a3bbf30ab8b2258346
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-03-14 06:51:32 +0300

    style:make changes on hover effect

--- js/script.js
+++ js/script.js
@@ -32,33 +32,66 @@ $(".work1").hover(function(){
     $(".work1-text").show();
     $(".work1-text h3").show();
 })
+$(".work1").mouseleave(function(){
+    $(".work1-text").hide();
+    $(".work1-text h3").hide();
+})
+
 $(".work2").hover(function(){
     $(".work2-text").show();
     $(".work2-text h3").show();
+})
+$(".work2").mouseleave(function(){
+    $(".work2-text").hide();
+    $(".work2-text h3").hide();
 })
 $(".work3").hover(function(){
     $(".work3-text").show();
     $(".work3-text h3").show();
 })
+$(".work3").mouseleave(function(){
+    $(".work3-text").hide();
+    $(".work3-text h3").hide();
+})
 $(".work4").hover(function(){
     $(".work4-text").show();
     $(".work4-text h3").show();
+})
+$(".work4").mouseleave(function(){
+    $(".work4-text").hide();
+    $(".work4-text h3").hide();
 })
 $(".work5").hover(function(){
     $(".work5-text").show();
     $(".work5-text h3").show();
 })
+$(".work5").mouseleave(function(){
+    $(".work5-text").hide();
+    $(".work5-text h3").hide();
+})
 $(".work6").hover(function(){
     $(".work6-text").show();
     $(".work6-text h3").show();
+})
+$(".work6").mouseleave(function(){
+    $(".work6-text").hide();
+    $(".work6-text h3").hide();
 })
 $(".work7").hover(function(){
     $(".work7-text").show();
     $(".work7-text h3").show();
 })
+$(".work7").mouseleave(function(){
+    $(".work7-text").hide();
+    $(".work7-text h3").hide();
+})
 $(".work8").hover(function(){
     $(".work8-text").show();
     $(".work8-text h3").show();
+})
+$(".work8").mouseleave(function(){
+    $(".work8-text").hide();
+    $(".work8-text h3").hide();
 })
 
  
